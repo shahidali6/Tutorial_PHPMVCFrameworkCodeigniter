c38bd3671f9ce190ba2401ecdba06bfb2dcd18af
Shahid Saddique <shahidali6@gmail.com>
2019-03-12 08:39:44 +0500

    Update Home.php

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,9 +1,9 @@
 <?php
 
-class Home extends CI_Controller{
-
+class Home extends CI_Controller
+{
     public function index()
     {
-        echo "this is home controller";
+        $this->load->view('home_view');
     }
 }
