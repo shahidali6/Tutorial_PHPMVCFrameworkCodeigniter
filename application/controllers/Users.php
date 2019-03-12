7215652530393eb5e5197a1c91b7d47aaae92eda
Shahid Saddique <shahidali6@gmail.com>
2019-03-12 12:09:27 +0500

    database configuration added.

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -5,8 +5,8 @@ class Users extends CI_Controller
 
     public function show()
     {
-        $this->load->model('user_model');
+        // $this->load->model('user_model');
 
-        $this->user_model->get_users();
+        $result =  $this->user_model->get_users();
     }
 }
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -2,8 +2,15 @@
 
 class User_model extends CI_Model
 {
- public function get_users()
- {
-     
- }
+    public function get_users()
+    {
+        // $config['hostname'] = 'localhost';
+        // $config['username'] = 'root';
+        // $config['password'] = '';
+        // $config['database'] = 'phpmvcframeworkcodeigniter';
+
+        // $connection = $this->load->database($config);
+
+        // $connection_2 = $this->load->database($config_2);
+    }
 }
