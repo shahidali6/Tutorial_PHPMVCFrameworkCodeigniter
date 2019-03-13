612544f4a64fea465783555008184a997732bf99
Shahid Saddique <shahidali6@gmail.com>
2019-03-13 12:36:34 +0500

    016 Conditions in our Queries lecture done

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -4,7 +4,10 @@ class User_model extends CI_Model
 {
     public function get_users($user_id)
     {
-        $this->db->where('id', $user_id);        
+$this->db->where(['id' => $user_id]);
+
+
+        //$this->db->where('id', $user_id);        
         $query = $this->db->get('users');
 
         return $query->result();
